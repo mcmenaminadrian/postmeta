c28e54e366815ade85151960c98679d2a2f47e06
Adrian McMenamin <adrianmcmenamin@gmail.com>
2013-01-13 23:16:53 +0000

    first commit

---
+++ metaedit/metaedit.groovy
@@ -0,0 +1,39 @@
+package metaedit
+
+class MetaPostEditor{
+	def metaPostFilePath
+	
+	def edit()
+	{
+		return 0
+	}
+	
+	
+}
+
+def metaCli = new CliBuilder
+	(usage: 'metaedit [options] <file>')
+	
+metaCli.u(longOpt: 'usage', 'show this information')
+metaCli.f(longOpt:'file',
+	'MetaPost file to edit')
+metaCli.header = "Edit and process MetaPost files, copyright Adrian McMenamin, 2013, licensed under GPL v3"
+	
+def metaParse = metaCli.parse(args)
+
+if (metaParse.u)
+	metaCli.usage
+else {
+	def metaEditor
+	
+	if (args.size() == 0)
+		metaEditor = new MetaPostEditor()
+	else {
+		def fileToEdit
+		if (metaCli.f)
+			metaEditor = new MetaPostEditor(metaPostFilePath:metaCli.f)
+		else
+			metaEditor = new MetaPostEditor(metaPostFilePath:args[args.size() - 1])
+	}
+	metaEditor.edit()	
+}	
